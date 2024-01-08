e3a7c5bbb835ebf3c284a49446f64cb6a3bddef7
nimilioum <nimilioum@gmail.com>
2024-01-08 14:48:09 +0330

    Feat: updated profile model, added bio and avatar

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,5 @@
-from django.db import models
-from utils.models import Model
+from django.db import models, IntegrityError
+from utils import Model, DomainException
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -8,3 +8,17 @@ User = get_user_model()
 class Profile(Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False, blank=True)
+    bio = models.TextField(max_length=500, null=True, blank=True)
+    avatar = models.ImageField(upload_to='images/avatars', null=True, blank=True)
+
+    def follow(self, user: User):
+        try:
+            self.follows.add(user.profile)
+        except IntegrityError:
+            raise DomainException('User is already followed')
+
+    def unfollow(self, user: User):
+        self.follows.remove(user.profile)
+
+    def is_followed(self, user: User):
+        return Profile.objects.filter(follows=user.profile, id=self.id).exists()
