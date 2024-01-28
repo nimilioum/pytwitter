b2a19202d7012df0d9c44771dba1452a4246008e
nimilioum <nimilioum@gmail.com>
2024-01-28 23:23:30 +0330

    fix: added default profile

--- auth_users/models.py
+++ auth_users/models.py
@@ -6,6 +6,6 @@ User = get_user_model()
 
 
 class Suspend(Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True, related_name='suspends')
     from_date = models.DateTimeField(auto_now_add=True)
     to_date = models.DateTimeField()
