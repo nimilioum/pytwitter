05c3cb14fc7867c12e63fe9b11d25b20ffd5f74b
nimilioum <nimilioum@gmail.com>
2024-01-28 17:18:35 +0330

    refactor: added api path

--- pytwitter/urls.py
+++ pytwitter/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
-    path('user/', include('posts.urls.user')),
-    path('auth/', include('auth_users.urls')),
-    path('posts/', include('posts.urls.post')),
-    path('hashtags/', include('posts.urls.hashtag'))
+    path('api/user/', include('posts.urls.user')),
+    path('api/auth/', include('auth_users.urls')),
+    path('api/posts/', include('posts.urls.post')),
+    path('api/hashtags/', include('posts.urls.hashtag'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
